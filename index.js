1abdddddc2b17e2d4e50994a615f370e20884e53
Dharshanaa SR <dharshanaasr04@gmail.com>
2026-06-06 15:44:32 +0530

    env setup

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
-const PORT = 5000;
 const mongoose = require('mongoose');
 const userRoutes = require('./Routes/User');
-
+const dotenv = require('dotenv');
+dotenv.config();
 app.use(express.json());
 app.use('/user', userRoutes);
 
@@ -11,10 +11,10 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
 });
-mongoose.connect('mongodb://vinsup:vinsup@ac-rocumns-shard-00-00.vnd2hfr.mongodb.net:27017,ac-rocumns-shard-00-01.vnd2hfr.mongodb.net:27017,ac-rocumns-shard-00-02.vnd2hfr.mongodb.net:27017/?ssl=true&replicaSet=atlas-10ts8h-shard-0&authSource=admin&appName=Cluster0')
+mongoose.connect(process.env.MONGO_URI)
     .then(() => {
         console.log('Connected to MongoDB');
     })
