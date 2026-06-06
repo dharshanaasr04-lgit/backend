44ba611350658d8e16c6ef13577e2d082fe361aa
Dharshanaa SR <dharshanaasr04@gmail.com>
2026-06-06 15:34:33 +0530

    atlas connection

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
-mongoose.connect('mongodb://localhost:27017/Batch13')
+mongoose.connect('mongodb://vinsup:vinsup@ac-rocumns-shard-00-00.vnd2hfr.mongodb.net:27017,ac-rocumns-shard-00-01.vnd2hfr.mongodb.net:27017,ac-rocumns-shard-00-02.vnd2hfr.mongodb.net:27017/?ssl=true&replicaSet=atlas-10ts8h-shard-0&authSource=admin&appName=Cluster0')
     .then(() => {
         console.log('Connected to MongoDB');
     })
